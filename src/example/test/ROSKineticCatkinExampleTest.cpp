ffd1946ca47065e433caf2bb59c8fa4d0e04645c
Edward Beech <edward.beech2@bhpbilliton.com>
2019-05-17 09:01:36 +0800

    Break tests into arrange/act/assert

--- src/example/test/ROSKineticCatkinExampleTest.cpp
+++ src/example/test/ROSKineticCatkinExampleTest.cpp
@@ -16,40 +16,44 @@ const int permittedScores[3] = {420, 1337, 8008135};
 
 // note the TM4J test case number (IdeTnnnn); if you copy and paste this code, be sure to update or remove this
 TEST(SomeTestSuite, testRTPSTopicPublisherIdeT2859) {
+
+    // arrange
     boost::shared_ptr<const example::ExampleMessage> sharedMsg = ros::topic::waitForMessage<example::ExampleMessage>(
             TOPIC_NAME,
             ros::Duration(3.0)
     );
 
-    ASSERT_NE(nullptr, sharedMsg);
+    ASSERT_NE(nullptr, sharedMsg) << "message not on topic after 3 seconds"; // pre-check for arrange
 
+    // act
     example::ExampleMessage msg = *sharedMsg;
 
-    ASSERT_EQ(firstName, msg.first_name);
-    ASSERT_EQ(lastName, msg.last_name);
-    ASSERT_LT(minAge, msg.age);
-
+    // assert
     bool found = false;
     for (int i = 0; i < sizeof(permittedScores); i++) {
         if (permittedScores[i] == msg.score) {
             found = true;
         }
     }
-    ASSERT_TRUE(found);
+
+    ASSERT_EQ(firstName, msg.first_name) << "msg.first_name was not " << firstName;
+    ASSERT_EQ(lastName, msg.last_name) << "msg.last_name was not " << lastName;
+    ASSERT_LT(minAge, msg.age) << "msg.age was not greater than" << minAge;
+    ASSERT_TRUE(found) << "response.score was not one of 420, 1337 or 8008135";
 }
 
 // note the TM4J test case number (IdeTnnnn); if you copy and paste this code, be sure to update or remove this
 TEST(SomeTestSuite, testServiceHostIdeT2860) {
+
+    // arrange
     boost::shared_ptr<const example::ExampleMessage> sharedMsg = ros::topic::waitForMessage<example::ExampleMessage>(
             TOPIC_NAME,
             ros::Duration(3.0)
     );
 
-    ASSERT_NE(nullptr, sharedMsg);
+    ASSERT_NE(nullptr, sharedMsg) << "message not on topic after 3 seconds"; // pre-check for arrange
 
     bool serviceAvailable = ros::service::waitForService(SERVICE_NAME, ros::Duration(3.0));
-
-    ASSERT_TRUE(serviceAvailable);
 
     ros::NodeHandle n = *nh;
 
@@ -60,19 +64,23 @@ TEST(SomeTestSuite, testServiceHostIdeT2860) {
     srv.request.first_name = firstName;
     srv.request.last_name = lastName;
 
-    ASSERT_TRUE(client.call(srv));
+    // act
+    bool callPassed = client.call(srv);
 
-    ASSERT_EQ(srv.response.first_name, firstName);
-    ASSERT_EQ(srv.response.last_name, lastName);
-    ASSERT_LT(minAge, srv.response.age);
-
+    // assert
     bool found = false;
     for (int i = 0; i < sizeof(permittedScores); i++) {
         if (permittedScores[i] == srv.response.score) {
             found = true;
         }
     }
-    ASSERT_TRUE(found);
+
+    ASSERT_TRUE(serviceAvailable) << "service not available after 3 seconds";
+    ASSERT_TRUE(callPassed) << "service call failed";
+    ASSERT_EQ(srv.response.first_name, firstName) << "response.first_name was not " << firstName;
+    ASSERT_EQ(srv.response.last_name, lastName) << "response.last_name was not " << lastName;
+    ASSERT_LT(minAge, srv.response.age) << "response.age was not greater than" << minAge;
+    ASSERT_TRUE(found) << "response.score was not one of 420, 1337 or 8008135";
 }
 
 int main(int argc, char **argv) {
--- src/example/test/ros_kinetic_catkin_example_test.py
+++ src/example/test/ros_kinetic_catkin_example_test.py
@@ -23,27 +23,33 @@ class TopicPublishSubscriberTest(unittest.TestCase):
 
     # note the TM4J test case number (ide_tnnnn); if you copy and paste this code, be sure to update or remove this
     def test_topic_publisher_ide_t2857(self):
+        # arrange
+
+        # act
         data = rospy.wait_for_message(TOPIC_NAME, ExampleMessage, timeout=3)
 
-        self.assertEqual(_first_name, data.first_name)
-        self.assertEqual(_last_name, data.last_name)
-        self.assertLess(_min_age, data.age)
-        self.assertIn(data.score, _permitted_scores)
+        # assert
+        self.assertEqual(_first_name, data.first_name, 'data.first_name was not {}'.format(_first_name))
+        self.assertEqual(_last_name, data.last_name, 'data.last_name was not {}'.format(_last_name))
+        self.assertLess(_min_age, data.age, 'data.age was not greater than {}'.format(_min_age))
+        self.assertIn(data.score, _permitted_scores, 'data.scores was not in {}'.format(_permitted_scores))
 
     # note the TM4J test case number (ide_tnnnn); if you copy and paste this code, be sure to update or remove this
     def test_service_hoster_ide_t2858(self):
+        # arrange
         _ = rospy.wait_for_message(TOPIC_NAME, ExampleMessage, timeout=3)
 
         rospy.wait_for_service(SERVICE_NAME, 1)
 
         service_method = rospy.ServiceProxy(SERVICE_NAME, ExampleService)
 
+        # act
         data = service_method(_first_name, _last_name)
 
-        self.assert_(_first_name, data.first_name)
-        self.assert_(_last_name, data.last_name)
-        self.assertLess(_min_age, data.age)
-        self.assertIn(data.score, _permitted_scores)
+        # assert
+        self.assertEqual(_first_name, data.first_name, 'data.first_name was not {}'.format(_first_name))
+        self.assertEqual(_last_name, data.last_name, 'data.last_name was not {}'.format(_last_name))
+        self.assertLess(_min_age, data.age, 'data.age was not greater than {}'.format(_min_age))
 
 
 class TopicPublishSubscriberSuite(unittest.TestSuite):
